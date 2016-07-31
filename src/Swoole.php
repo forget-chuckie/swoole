496d8efc2655cd4ebb569b61d226a5c216e03636
陈鸿 <747825455@qq.com>
2016-07-31 15:02:37 +0800

    test

---
+++ src/Swoole.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chenhong
+ * Date: 16/7/31
+ * Time: 下午3:01
+ */
+namespace Swoole;
+
+class Swoole
+{
+    public function __construct()
+    {
+        echo 'success';
+    }
+}
